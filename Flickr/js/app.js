187508d0bc9f0bbde0d921a26416b9973ac34914
Joe Keohah <jkeohan@gmail.com>
2016-03-01 11:31:58 -0500

    flickrApi

--- Flickr/js/app.js
+++ Flickr/js/app.js
@@ -4,55 +4,59 @@ myApp.controller('flickrCtrl', ['$scope','$http',function($scope,$http){
 	'use strict';
 	$scope.searchMsg = false;
 	$scope.resultMsg = false;
+	$scope.errorMsg = false;
 
-	$scope.submit = function() { 
-		$scope.searchQueryCopy = $scope.searchQuery;
-		$scope.resultMsg = false;
-		$scope.searchMsg = true;
-
-		$scope.searchQuery = ' ';
-		var url = "https://api.flickr.com/services/rest";
-		var request = {
-	    method: 'flickr.photos.search',
-	    api_key: '1f248d8314f6e79c501fdb818513f0ff',
-	    tags: $scope.searchQueryCopy,
-	    format: 'json',
-	    jsoncallback: 'JSON_CALLBACK'
-	    // nojsoncallback: '1'
+	$scope.submit = function() {
+		if($scope.searchForm.$invalid) {
+			console.log($scope.searchForm.$invalid)
 		}
- 
-       $http({
+		else {
+			$scope.searchQueryCopy = $scope.searchQuery;
+			$scope.searchQuery = ' ';
+			$scope.resultMsg = false;
+			$scope.searchMsg = true;
+
+			var url = "https://api.flickr.com/services/rest";
+			var request = {
+		    method: 'flickr.photos.search',
+		    api_key: '1f248d8314f6e79c501fdb818513f0ff',
+		    tags: $scope.searchQueryCopy,
+		    format: 'json',
+		    jsoncallback: 'JSON_CALLBACK'
+		    // nojsoncallback: '1'
+			}
+
+			 $http({
 		      method: 'JSONP',
 		      url: url,
 		      params: request
 		    })
-       .then(function(result) {
-       		$scope.searchMsg = false;
+			 	.success(function(result) {
+			 		console.log("success")
+			 		updateView(result)
+				 })
+			 	.error(function(result) { 
+			 		console.log(('error'))
+			 		errorView()
+			 	})
+			}//submit()
+
+			function updateView(result) {
+					console.log(result)
+					$scope.searchMsg = false;
        		$scope.resultsMsg = true;
-		      $scope.result = result.data.photos.photo
-		      console.log($scope.result.length)
+		      $scope.result = result.photos.photo
 		      $scope.results = $scope.result.length
+			}
 
-		    },
-		    function(result) {
-		      alert('error');
-		    });
-          
-		// 'https://farm{farmId}.staticflickr.com/{serverId}/{id}_{secret}.jpg'
-		//http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
-			// https://farm2.staticflickr.com/1456/25022884939_c284ab498f.(jpg|gif|png)
-			//This works...
-			//https://farm2.staticflickr.com/1456/25022884939_c284ab498f.jpg
-	}
-}]);
+			function errorView(){
+				$scope.searchMsg = false;
 
-// https://api.flickr.com/services/rest/
-// ?format=json
-// &method=flickr.photos.search
-// &tags=dog
-// &api_key=1f248d8314f6e79c501fdb818513f0ff
-// &nojsoncallback=1
+			}
+		}//flickrCtrl
+}]);	
 
+//JSON RETURNED BY FLICKR API CALL
 // id: "25022884939",
 // owner: "43046057@N05",
 // secret: "c284ab498f",
@@ -63,9 +67,20 @@ myApp.controller('flickrCtrl', ['$scope','$http',function($scope,$http){
 // isfriend: 0,
 // isfamily: 0
 
+//FORMAT FOR DISPLAYING IMG
+// https://farm{farmId}.staticflickr.com/{serverId}/{id}_{secret}.jpg
+// https://farm2.staticflickr.com/1456/25022884939_c284ab498f.jpg
+		
+ 
 // ISSUE: Cannot read property of jsonp
 // RESOLUTION: Removed the jsonp from $http.jsonp and then updated code to use thinkfuls $http request format. 
 
 //ISSUE: resultsMsg not changing until after promise which means both msgs are being displayed.  Only happens on 
 //       running a subsequent query
 //RESOLUTION: 
+
+//ISSUE: unable to animate the searchMsg div or images.  would like to stagger the pics as they enter
+//RESOLUTION: 
+//REVIEW ARTICLE WITH MOHAMMAD
+//TO THEN() OR TO SUCCESS()
+//http://www.peterbe.com/plog/promises-with-$http
